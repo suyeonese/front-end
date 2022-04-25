53c34565d8a2307e401e1f71be0a8abfa4be8d3f
suyeonese <suyeon.here@gmail.com>
2022-04-25 14:16:13 +0900

    JavaScript

---
+++ _js/01_basic/05_function.js
@@ -0,0 +1,7 @@
+console.log("---------------------------")
+console.log("fucntion.js에서 실행되는 코드");
+console.log("---------------------------")
+
+function outerFunc() {
+    console.log("function.js의 outerFunc에서 실행되는 코드");
+}
---
+++ _js/01_basic/05_function2.js
@@ -0,0 +1,15 @@
+function myFunc1() {
+    console.log('너무 선언식이 많으면 따로 js파일을 만ㄷ르어서 선언식을 모아둔다!')
+}
+function myFunc2() {
+    console.log('너무 선언식이 많으면 따로 js파일을 만ㄷ르어서 선언식을 모아둔다!')
+}
+function myFunc3() {
+    console.log('너무 선언식이 많으면 따로 js파일을 만ㄷ르어서 선언식을 모아둔다!')
+}
+function myFunc4() {
+    console.log('너무 선언식이 많으면 따로 js파일을 만ㄷ르어서 선언식을 모아둔다!')
+}
+function myFunc5() {
+    console.log('너무 선언식이 많으면 따로 js파일을 만ㄷ르어서 선언식을 모아둔다!')
+}
